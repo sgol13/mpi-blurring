bc54bc92e9e660c884ed8564f2519c94eff0b113
Szymon Golebiowski <szymon13gol@gmail.com>
2022-02-22 01:55:12 +0100

    cmake compilation

---
+++ build/CMakeFiles/exe.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/szymon/code/projects/mpi-blurring/src/Alloc.cpp" "/home/szymon/code/projects/mpi-blurring/build/CMakeFiles/exe.dir/src/Alloc.cpp.o"
+  "/home/szymon/code/projects/mpi-blurring/src/ArithmeticMeanFunction.cpp" "/home/szymon/code/projects/mpi-blurring/build/CMakeFiles/exe.dir/src/ArithmeticMeanFunction.cpp.o"
+  "/home/szymon/code/projects/mpi-blurring/src/DataProcessor.cpp" "/home/szymon/code/projects/mpi-blurring/build/CMakeFiles/exe.dir/src/DataProcessor.cpp.o"
+  "/home/szymon/code/projects/mpi-blurring/src/InitialDataGenerator.cpp" "/home/szymon/code/projects/mpi-blurring/build/CMakeFiles/exe.dir/src/InitialDataGenerator.cpp.o"
+  "/home/szymon/code/projects/mpi-blurring/src/MPIDataProcessor.cpp" "/home/szymon/code/projects/mpi-blurring/build/CMakeFiles/exe.dir/src/MPIDataProcessor.cpp.o"
+  "/home/szymon/code/projects/mpi-blurring/src/MagicFuntion.cpp" "/home/szymon/code/projects/mpi-blurring/build/CMakeFiles/exe.dir/src/MagicFuntion.cpp.o"
+  "/home/szymon/code/projects/mpi-blurring/src/PRIR2021_Z01.cpp" "/home/szymon/code/projects/mpi-blurring/build/CMakeFiles/exe.dir/src/PRIR2021_Z01.cpp.o"
+  "/home/szymon/code/projects/mpi-blurring/src/SequentialDataProcessor.cpp" "/home/szymon/code/projects/mpi-blurring/build/CMakeFiles/exe.dir/src/SequentialDataProcessor.cpp.o"
+  "/home/szymon/code/projects/mpi-blurring/src/SimpleInitialDataGenerator.cpp" "/home/szymon/code/projects/mpi-blurring/build/CMakeFiles/exe.dir/src/SimpleInitialDataGenerator.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/exe.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/exe.dir/src/Alloc.cpp.o"
+  "CMakeFiles/exe.dir/src/ArithmeticMeanFunction.cpp.o"
+  "CMakeFiles/exe.dir/src/DataProcessor.cpp.o"
+  "CMakeFiles/exe.dir/src/InitialDataGenerator.cpp.o"
+  "CMakeFiles/exe.dir/src/MPIDataProcessor.cpp.o"
+  "CMakeFiles/exe.dir/src/MagicFuntion.cpp.o"
+  "CMakeFiles/exe.dir/src/PRIR2021_Z01.cpp.o"
+  "CMakeFiles/exe.dir/src/SequentialDataProcessor.cpp.o"
+  "CMakeFiles/exe.dir/src/SimpleInitialDataGenerator.cpp.o"
+  "exe.pdb"
+  "exe"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/exe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
